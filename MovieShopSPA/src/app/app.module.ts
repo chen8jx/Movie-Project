8b4e7e833dcd0cfb2978a16bc33fa1d99db4402b
Jingyi Chen <chen.jingyi1@northeastern.edu>
2020-12-08 19:35:10 -0500

    MovieShopSPA

---
+++ MovieShopSPA/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './core/layout/header/header.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { GenresComponent } from './genres/genres.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    GenresComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
